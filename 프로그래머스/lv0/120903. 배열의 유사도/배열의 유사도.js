9b7e274d5ae9b4994e6aae1daffd1f23c1297618
lain-alice <119744952+lain-alice@users.noreply.github.com>
2023-09-05 14:35:25 +0900

    [level 0] Title: 배열의 유사도, Time: 0.05 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120903. 배열의 유사도/배열의 유사도.js
@@ -0,0 +1,11 @@
+function solution(s1, s2) {
+    let answer = 0;
+    
+    for (i = 0; i < s1.length; i++) {
+        for (j = 0; j < s2.length; j++) {
+            if (s1[i] === s2[j]) answer = answer + 1;
+        }
+    }
+    
+    return answer;
+}
