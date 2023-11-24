2c6fcb8536c4ae0c5392ff062c770d0ba22d5c70
lain-alice <119744952+lain-alice@users.noreply.github.com>
2023-11-24 23:58:23 +0900

    [level unrated] Title: [PCCP 기출문제] 1번, Time: 0.08 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/250137. ［PCCP 기출문제］ 1번/［PCCP 기출문제］ 1번.js
@@ -0,0 +1,32 @@
+// function solution(bandage, health, attacks) {
+//     let t = bandage[0];
+//     let x = bandage[1];
+//     let y = bandage[2];
+    
+//     let heal = t * x + y;
+    
+//     // 
+    
+//     // health - attacks + heal
+// }
+
+function solution(bandage, health, attacks) {
+    const maxHealth = health;
+    const [t, x, y] = bandage;
+    
+    let lastAttackTime = 0;
+    
+    for (const [attackTime, damage] of attacks) {
+        const timeDiff = attackTime - lastAttackTime - 1;
+        const heal = timeDiff * x + Math.floor(timeDiff / t) * y;
+        health = Math.min(health + heal, maxHealth);
+        
+        health -= damage;
+        
+        if (health <= 0) return -1;
+        
+        lastAttackTime = attackTime;
+    }
+    
+    return health;
+}
