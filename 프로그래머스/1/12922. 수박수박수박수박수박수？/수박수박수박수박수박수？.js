2d2fa4721369c1cc0fb43e2ded96d2499cb6b6d2
lain-alice <119744952+lain-alice@users.noreply.github.com>
2024-03-12 19:04:32 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 0.04 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.js
+++ 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.js
@@ -1,15 +1,9 @@
 function solution(n) {
-    let watermelon = [];
+    let watermelon = '';
     
-    for ( i = 0; i < n; i++ ) {
-        
-        if ((i + 1) % 2 === 0) {
-            watermelon[i] = '박'
-        } else {
-            watermelon[i] = '수'
-        }
-        
+    for ( i = 0; i < n; i++ ) {        
+        ((i + 1) % 2 === 0) ? watermelon += '박' : watermelon += '수';
     }
     
-    return watermelon.join('');
+    return watermelon;
 }
