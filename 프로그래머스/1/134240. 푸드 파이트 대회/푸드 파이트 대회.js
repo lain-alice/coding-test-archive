1ce91c076da3add8897fa8dac7148e72ad49a993
lain-alice <119744952+lain-alice@users.noreply.github.com>
2024-03-14 18:56:12 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.js
@@ -0,0 +1,16 @@
+function solution(food) {
+    
+    let table = '';
+    
+    for ( i = 1 ; i <= food.length ; i++ ){
+                       
+        let count = Math.floor(food[i] / 2)
+        
+        table += String(i).repeat(count)
+        
+    }
+    
+    let answer = table + '0' + [...table].reverse().join('')
+    
+    return answer;
+}
