bd40fe6040ccb693059cbfb68ff68872378781d8
lain-alice <119744952+lain-alice@users.noreply.github.com>
2024-03-15 18:46:10 +0900

    [level 1] Title: 로또의 최고 순위와 최저 순위, Time: 0.10 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.js
@@ -0,0 +1,26 @@
+function solution(lottos, win_nums) {
+    
+    
+    let sameNums = lottos.filter( a => win_nums.includes(a) )
+    let erasedNums = lottos.filter( a => a === 0 )
+    
+    let minRank = 0
+    let maxRank = 0
+    
+    if (sameNums.length <= 1) {
+        minRank = 6
+    } else {
+        minRank = 7 - sameNums.length
+    } 
+    
+    if (sameNums.length <= 1 && erasedNums.length === 0) {
+        maxRank = 6
+    } else {
+        maxRank = 7 - (sameNums.length + erasedNums.length)
+    } 
+    
+    let answer = [];
+    answer.push(maxRank, minRank)
+    
+    return answer;
+}
