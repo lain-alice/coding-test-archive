faf3c189f01d0cb034c36057eb1806a5c9be4401
lain-alice <119744952+lain-alice@users.noreply.github.com>
2024-03-29 17:21:23 +0900

    [Bronze III] Title: 알고리즘 수업 - 알고리즘의 수행 시간 4, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/24265. 알고리즘 수업 － 알고리즘의 수행 시간 4/알고리즘 수업 － 알고리즘의 수행 시간 4.py
@@ -0,0 +1,17 @@
+n = int(input())
+print((n - 1) * n // 2)
+print(2)
+
+# 11 12 22 13 23 33 
+
+# def MenOfPassion(A, n) :
+#     sum = 0
+#     for i in range(n):        
+#         for j in range(i+1):
+#             sum += A[i] * A[j]
+#     return sum
+
+# print(MenOfPassion([1, 2, 3], 3))
+
+# 리스트 A에서 나올 수 있는 모든 숫자 쌍을 서로 곱하고 다 합친다
+# 이중반복문의 시간복잡도 O(n^2)
