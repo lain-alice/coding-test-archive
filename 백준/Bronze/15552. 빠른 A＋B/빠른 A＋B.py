efd007598018acd0022ae1d1ac1f78a1f36b79c5
lain-alice <119744952+lain-alice@users.noreply.github.com>
2024-04-02 15:54:49 +0900

    [Bronze IV] Title: 빠른 A+B, Time: 1296 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.py
+++ 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.py
@@ -2,10 +2,8 @@ import sys
 input = sys.stdin.readline
 
 n = int(input())
-sum_list = [list(map(int, input().split())) for i in range(n)]
 
-all_sum = []
 for i in range(n):
-    all_sum.append(sum(sum_list[i]))
-    print(all_sum[i])
+    a, b = map(int, input().split())
+    print(a + b)
 
