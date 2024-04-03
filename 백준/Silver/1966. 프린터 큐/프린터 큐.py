15d7915d5c49891b9bb89501ea403622fd5efc66
lain-alice <119744952+lain-alice@users.noreply.github.com>
2024-04-04 01:24:09 +0900

    [Silver III] Title: 프린터 큐, Time: 64 ms, Memory: 34052 KB -BaekjoonHub

---
+++ 백준/Silver/1966. 프린터 큐/프린터 큐.py
@@ -0,0 +1,32 @@
+# 큐는 말 그대로 줄 서기, 먼저 온 애가 먼저 간다(선입선출)
+# 프린트 순서는 큐, 대신 명령 순서 말고 중요도
+
+from collections import deque
+import sys
+input = sys.stdin.readline
+
+t = int(input())
+
+for _ in range(t):
+    printer = deque()
+    n, m = map(int, input().split())  # 6 0
+    docs = list(map(int, input().split()))  # [1, 1, 9, 1, 1, 1]
+
+    for index, prior in enumerate(docs):
+        printer.append((prior, index))  # [(1, 0), (1, 1), (9, 2)...]
+
+    docs.sort()  # [(1, 0), (1, 1),... (9, 2)] 문서가 중요도 순서대로 정렬됨
+    cnt = 0
+
+    while printer:  # 프린터에 문서가 있으면
+        prior, index = printer.popleft()
+        if prior != docs[-1]:
+            printer.append((prior, index))
+        else:
+            cnt += 1
+            docs.pop()
+            if index == m:
+                print(cnt)
+                break
+
+
